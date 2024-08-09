d8677295270d83cc21629687ca864a223a031fc4
kanipan314 <k023g1020@g.neec.ac.jp>
2024-08-09 13:40:04 +0900

    プロジェクト ファイルを追加します。

---
+++ MT3-09/main.cpp
@@ -0,0 +1,102 @@
+#include <Novice.h>
+#include "MT.h"
+#include "imgui.h"
+
+const char kWindowTitle[] = "学籍番号";
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
+
+	Segment segment{ {-2.0f, -1.0f, 0.0f}, {3.0f, 2.0f, 2.0f} };
+	Vector3 point{ -1.5f, 0.6f, 0.6f };
+
+	Vector3 project = {};
+	Vector3 closestPoint = {};
+	Sphere pointSpere{ point, 0.01f };
+	Sphere closestPointSphere{ closestPoint, 0.01f };
+
+	Vector3 rotate = {};
+	Vector3 translate = {};
+	Vector3 cameraRotate = { 0.26f, 0.0f, 0.0f };
+	Vector3 cameraTranslate = { 0.0f, 1.9f, -6.49f };
+	Vector3 cameraPosition = { 0,0,-5.0f };
+
+	Vector3 start{};
+	Vector3 end{};
+
+	Matrix4x4 worldMatrix = {};
+	Matrix4x4 cameraMatrix = {};
+	Matrix4x4 viewMatrix = {};
+	Matrix4x4 projectionMatrix = {};
+	Matrix4x4 worldViewProjectionMatrix = {};
+	Matrix4x4 viewportMatrix = {};
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+		worldMatrix = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, rotate, translate);
+		cameraMatrix = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, cameraRotate, cameraTranslate);
+		viewMatrix = Inverse(cameraMatrix);
+		projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+		worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
+		viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+
+		project = Project(Subtract(point, segment.origin), segment.diff);
+		closestPoint = ClosestPoint(point, segment);
+
+		pointSpere = { point, 0.01f };
+		closestPointSphere = { closestPoint, 0.01f };
+
+		ImGui::DragFloat3("Project", &project.x, 0.01f);
+		ImGui::DragFloat3("closestPoint", &closestPoint.x, 0.01f);
+		ImGui::DragFloat3("Point", &point.x, 0.01f);
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+
+		DrawSphere(pointSpere, worldViewProjectionMatrix, viewportMatrix, RED);
+		DrawSphere(closestPointSphere, worldViewProjectionMatrix, viewportMatrix, BLACK);
+
+		DrawGrid(worldViewProjectionMatrix, viewportMatrix);
+
+		start = Transform(Transform(segment.origin, worldViewProjectionMatrix), viewportMatrix);
+		end = Transform(Transform(Add(segment.origin, segment.diff), worldViewProjectionMatrix), viewportMatrix);
+
+		Novice::DrawLine(int(start.x), int(start.y), int(end.x), int(end.y), WHITE);
+
+		/// 
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
